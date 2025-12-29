17d4a82b4b3c200aaf1f8aa4384529bde584641c
Hero Developers <134581766+Herosoftware01@users.noreply.github.com>
2025-12-29 19:58:25 +0530

    bala

--- src/page/ordsync.tsx
+++ src/page/ordsync.tsx
@@ -212,7 +212,7 @@ function Ordsync() {
         toolbarClick={toolbarClick}
       >
         <ColumnsDirective>
-          <ColumnDirective field="jobno_oms" headerText="Job No" width="150" />
+          <ColumnDirective field="jobno_oms" headerText="Job No11" width="150" />
           <ColumnDirective field="styleid" headerText="Style ID" width="120" />
           <ColumnDirective field="company_name" headerText="Company" width="200" />
           <ColumnDirective field="pono" headerText="PO No" width="120" />
